c1d8389620ee506adc5d40ef2c441ce06519993d
Iván <ivanrms371@gmail.com>
2025-03-11 01:34:17 -0300

    More duration to the h1 animation

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -33,7 +33,7 @@ export const About = () => {
             <motion.span
               initial={{ opacity: 0, y: -20 }}
               whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.3 }}
+              transition={{ duration: 0.5 }}
               viewport={{ once: true }}
               className="block"
             >
@@ -42,7 +42,7 @@ export const About = () => {
             <motion.span
               initial={{ opacity: 0, y: -20 }}
               whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.3, delay: 0.1 }}
+              transition={{ duration: 0.5, delay: 0.1 }}
               viewport={{ once: true }}
               className="block"
             >
